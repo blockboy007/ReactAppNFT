05bad96f51f90001b21e1195a947b2694207973e
Blockboy007 <70123625+blockboy007@users.noreply.github.com>
2022-09-05 13:45:01 -0700

    Add files via upload

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,6 +5,7 @@ import "./PostForm.css";
 // the const (props) will be set already, and the routing at the bottom of the page, in 'return'
 // will allow the data to be used in both instances.
 // wallet = addr1qx2zcd86jaglrg6jqxavv3u78yu8xkjhpytsweprylyufkrvtqenlfxlc74e6ak9urzy9s4rlk5802xhq0a8tzhnxynqwv5c77
+// addr1q9e96g3ctafpand3t8sa7p3sky8cuh03u6adkex4kjz39mt6s0wl2ej59gsmff47ak398r4hh9a2xp5hdyj6dgjdj64qq9rute
 
 export default function PostForm(props) {
   const [value, setValue] = useState('');
@@ -41,6 +42,22 @@ export default function PostForm(props) {
               setDataIsLoaded(true);
           });
   };
+  const loading = (
+    <h1 className="loading">Please wait while loading...</h1>
+  );
+  const fetchedData = (
+    items.map((item) => (
+     <div className="card mb-3" key={item.id}>
+       <img src={item.img} className="card-img-top" alt="..." />
+       <div className="card-body">
+         <h5 className="card-title">{item.asset_name}</h5>
+         <p className="card-text">ADA price: {item.nft_price}</p>
+         <p className="card-text">USD price: {item.usd_price}</p>
+         <p className="card-text"><small className="text-muted">Policy ID: {item.policy_id}</small></p>
+       </div>
+     </div>
+   ))
+ );
 
   if (!clicked) {
     return (
@@ -55,21 +72,9 @@ export default function PostForm(props) {
   } else {
     return (
       <div className="dataPage">
-          <h1> Fetch data from an api in react </h1>
-          <h2 className="walletName">Wallet Address: { value }</h2>
-               {
-                 items.map((item) => (
-                  <div key={item.id} className="carousel" data-bs-interval="1000000">
-                    <div className="d-flex justify-content-center">
-                      <img src={item.img} className="w-25" alt="..." />
-                    </div>
-                      <p className="lead mb-4 d-flex justify-content-center">Name: {item.asset_name}</p>
-                      <p className="lead mb-4 d-flex justify-content-center">ADA Price: {item.nft_price}</p>
-                      <p className="lead mb-4 d-flex justify-content-center">USD Price: {item.usd_price}</p>
-                      <p className="lead mb-4 d-flex justify-content-center">Policy ID: {item.policy_id}</p>
-                  </div>
-                ))
-              };
+          <h1>NFTs from your wallet address:</h1>
+          <h2 className="walletName">{ value }</h2>
+          {!dataIsLoaded ? loading : fetchedData}
       </div>
     );
   };
